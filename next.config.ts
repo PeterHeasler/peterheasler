a200747ad58c242eced000ba8914017dcecb4753
Peter Heasler <peter@getgreenline.co>
2025-10-14 15:34:00 -0500

    feat: add basePath and assetPrefix to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/peterheasler',
+  assetPrefix: '/peterheasler/',
 };
 
 export default nextConfig;
