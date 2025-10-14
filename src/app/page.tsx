59aefed18d68c6e21ff279249b18ba18fbe3d1a5
Peter Heasler <peter@getgreenline.co>
2025-10-14 14:28:39 -0500

    fix(blog): escape apostrophe in page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1 className="text-4xl font-bold">Peter Heasler's Blog</h1>
+        <h1 className="text-4xl font-bold">Peter Heasler&apos;s Blog</h1>
         <ul>
           {posts.map((post) => (
             <li key={post.slug}>
