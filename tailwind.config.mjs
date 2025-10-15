a271b0f6637c747dc30ecc940fb17bb69e148f55
Peter Heasler <peter@getgreenline.co>
2025-10-14 22:21:19 -0500

    fix: styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,7 @@
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,6 +10,8 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/typography')
+    typography,
   ],
-}
+};
+
+export default config;
