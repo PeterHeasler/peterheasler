15ecf447a2a1ef90cf5294ff881be5c547eb1adf
Peter Heasler <peter@getgreenline.co>
2025-10-16 13:51:05 -0500

    fix: console error for component mount

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -13,6 +13,15 @@ import {
 
 export function ThemeSwitcher() {
   const { setTheme } = useTheme()
+  const [mounted, setMounted] = React.useState(false)
+
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
 
   return (
     <DropdownMenu>
